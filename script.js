6bc7356d94d1927e1bf10fe2186c989843109879
Benjamin Lupa <147424978+BenjaminLupa@users.noreply.github.com>
2024-03-12 16:01:09 +0100

    feat: added variables

---
+++ script.js
@@ -0,0 +1,8 @@
+const apiKey = "981b2bcca4c56c67d791ae50ab9ba39c";
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const language = "de";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
